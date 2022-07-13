81f65e2ae7e7c6092333ca4e0b1d90c6f7a59583
Curtis Culpepper <curtis.culpepper@Curtiss-Air.lan1>
2022-07-13 19:05:57 -0400

    code up to 14 complete

--- problems/10-my-every.js
+++ problems/10-my-every.js
@@ -1,28 +1,33 @@
 /*******************************************************************************
-Write a function `myEvery` that accepts an array and a callback as arguments.
-The function should return a boolean indicating whether or not all elements of
-the array return true when passed into the callback.
-
-Do not use the built in Array#every.
-
-Examples:
-
-let isEven = function (num) {
-    return num % 2 === 0;
-};
-
-let hasO = function(string) {
-    return string.includes('o');
-};
-
-console.log(myEvery([4, 8, 6, 10], isEven));            // true
-console.log(myEvery([4, 7, 6, 10], isEven));            // false
-console.log(myEvery(['book', 'door'], hasO));           // true
-console.log(myEvery(['book', 'door', 'pen'], hasO));    // false
+ 
+ Do not use the built in Array#every.
+ 
+ Examples:
+ 
+ let isEven = function (num) {
+     return num % 2 === 0;
+    };
+    
+    let hasO = function(string) {
+        return string.includes('o');
+    };
+    console.log(myEvery([4, 8, 6, 10], isEven));            // true
+    console.log(myEvery([4, 7, 6, 10], isEven));            // false
+    console.log(myEvery(['book', 'door'], hasO));           // true
+    console.log(myEvery(['book', 'door', 'pen'], hasO));    // false
+    
+    Write a function `myEvery` that accepts an array and a callback as arguments.
+    The function should return a boolean indicating whether or not all elements of
+    the array return true when passed into the callback.
 *******************************************************************************/
 
-let myEvery = function() {
-
+let myEvery = function(arr, cb) {
+for (let el of arr) {
+    if (cb(el) === false) {
+        return false
+    }
+}
+return true;
 };
 
 
--- problems/13-min-value-callback.js
+++ problems/13-min-value-callback.js
@@ -10,10 +10,22 @@ console.log(minValueCallback([64, 25, 49, 9, 100], Math.sqrt));  // 3
 
 *******************************************************************************/
 
-let minValueCallback = function() {
-
+let minValueCallback = function(arr, cb) {
+    if (cb === undefined) {
+        return findSmallest(arr);
+    } else { return cb(findSmallest(arr))}
+    
 };
 
+function findSmallest(arr){
+    let smallest = Infinity;
+    for (let el of arr) {
+    if (el < smallest) {
+        smallest = el
+        }
+    }
+    return smallest;
+}
 
 
 
